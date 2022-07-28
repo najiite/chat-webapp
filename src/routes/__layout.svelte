98362e39513ee976817b46443a576b759027c897
Onajite Kelvin <onajiteokeh@gmail.com>
2022-07-28 02:16:31 +0100

    app setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+</script>
+
+  <Nav />
+  <div class="container mx-auto max-w-lg p-6">
+  <slot />
+  </div>
+  <Footer />
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import autoImport from 'sveltekit-autoimport';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [
+		autoImport({
+			components: ['./src/components'],
+		  }),
+		  sveltekit()
+		]
+};
+
+export default config;
